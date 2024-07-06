a8bca4eafe6202c83c5430114aa6523d4e86f245
AlexLeoChumak <chumak1106@gmail.com>
2024-07-06 21:37:13 +0300

    add register(data: RegisterRequestInterface)

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,21 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  register(data: RegisterRequestInterface): CurrentUserInterface {
-    return {
-      id: 1,
-      email: 'dfg@fgfdgd.rt',
-      createdAt: 'string',
-      updatedAt: 'ghhfhh',
-      username: 'Jon',
-      bio: null,
-      image: null,
-      token: 'jghgjhfgjgfj',
-    };
+  register(data: RegisterRequestInterface): Observable<any> {
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/users/1');
   }
 }
