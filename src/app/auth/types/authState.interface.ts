6e441346e846fe17b387b28009802404789b1ea9
AlexLeoChumak <chumak1106@gmail.com>
2024-07-08 19:41:38 +0300

    add new fields

--- src/app/auth/types/authState.interface.ts
+++ src/app/auth/types/authState.interface.ts
@@ -1,3 +1,8 @@
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+
 export interface AuthStateInterface {
+  currentUser: CurrentUserInterface | null;
   isSubmitting: boolean;
+  isLoggedIn: boolean | null;
+  validationErrors: any | null;
 }
