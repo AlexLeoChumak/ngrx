643656cad72bd908a4277a54683eb040ef0f0597
AlexLeoChumak <chumak1106@gmail.com>
2024-07-10 20:05:29 +0300

    add login(data: LoginRequestInterface)

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface';
-import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 import { Observable } from 'rxjs';
+import { LoginRequestInterface } from 'src/app/auth/types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(data: RegisterRequestInterface): Observable<any> {
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/users/1');
+  }
+
+  login(data: LoginRequestInterface): Observable<any> {
     return this.http.get<any>('https://jsonplaceholder.typicode.com/users/1');
   }
 }
