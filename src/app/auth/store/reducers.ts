c490d75c74308dd34870483cb7df3ec211809790
AlexLeoChumak <chumak1106@gmail.com>
2024-07-11 20:45:22 +0300

    create [Auth] Login

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,19 +1,78 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
+import {
+  loginAction,
+  loginFailureAction,
+  loginSuccessAction,
+} from 'src/app/auth/store/actions/login.action';
 
-import { registerAction } from 'src/app/auth/store/actions';
+import {
+  registeFailureAction,
+  registerAction,
+  registerSuccessAction,
+} from 'src/app/auth/store/actions/register.action';
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationErrors: null,
 };
 
 const authReducer = createReducer(
   initialState,
+
+  // [Auth] Register
   on(
     registerAction,
     (state): AuthStateInterface => ({
       ...state,
       isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+    })
+  ),
+  on(
+    registeFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
+    })
+  ),
+
+  // [Auth] Login
+  on(
+    loginAction,
+    (state): AuthStateInterface => ({
+      ...state,
+      isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    loginSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+    })
+  ),
+  on(
+    loginFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
     })
   )
 );
@@ -22,3 +81,5 @@ export const authFeature = createFeature({
   name: 'auth',
   reducer: authReducer,
 });
+
+//19
