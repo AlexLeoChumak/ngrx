c419955a95b14e383cb518272b6ce6afc2988da9
AlexLeoChumak <chumak1106@gmail.com>
2024-07-11 20:45:50 +0300

    create selectAuthState

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -1,3 +1,36 @@
+import { createSelector } from '@ngrx/store';
 import { authFeature } from 'src/app/auth/store/reducers';
+import { AuthStateInterface } from 'src/app/auth/types/authState.interface';
 
-export const { selectAuthState, selectIsSubmitting } = authFeature;
+//упрощенный вариант
+// export const { selectAuthState, selectIsSubmitting } = authFeature;
+
+export const selectAuthState = createSelector(
+  authFeature.selectAuthState,
+  (state: AuthStateInterface) => state
+);
+
+export const selectIsSubmitting = createSelector(
+  selectAuthState,
+  (state: AuthStateInterface) => state.isSubmitting
+);
+
+export const selectValidationErrors = createSelector(
+  selectAuthState,
+  (state: AuthStateInterface) => state.validationErrors
+);
+
+export const selectIsLoggedIn = createSelector(
+  selectAuthState,
+  (state: AuthStateInterface) => state.isLoggedIn
+);
+
+export const selectIsAnonymous = createSelector(
+  selectAuthState,
+  (state: AuthStateInterface) => state.isLoggedIn === false
+);
+
+export const selectCurrentUser = createSelector(
+  selectAuthState,
+  (state: AuthStateInterface) => state.currentUser
+);
