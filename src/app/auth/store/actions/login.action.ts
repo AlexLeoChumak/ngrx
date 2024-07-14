886eb3584fd676598b37dfe6be65af69f1a7039a
AlexLeoChumak <chumak1106@gmail.com>
2024-07-14 09:42:37 +0300

    create loginAction

---
+++ src/app/auth/store/actions/login.action.ts
@@ -0,0 +1,19 @@
+import { createAction, props } from '@ngrx/store';
+
+import { ActionTypes } from 'src/app/auth/store/actionTypes';
+import { LoginRequestInterface } from 'src/app/auth/types/loginRequest.interface';
+
+export const loginAction = createAction(
+  ActionTypes.LOGIN,
+  props<{ request: LoginRequestInterface }>()
+);
+
+export const loginSuccessAction = createAction(
+  ActionTypes.LOGIN_SUCCESS,
+  props<{ currentUser: any }>()
+);
+
+export const loginFailureAction = createAction(
+  ActionTypes.LOGIN_FAILURE,
+  props<{ errors: any }>()
+);
